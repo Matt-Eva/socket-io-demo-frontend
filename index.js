c562472eb7f178791d9661c6a6b6f2d904a63e27
Matt Eva <matteweva@gmail.com>
2023-07-05 21:22:46 -0700

    receiving broadcasts

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const socket = io("http://localhost:4000")
+const form = document.getElementById("message-form")
+const messageChain = document.getElementById("message-chain")
 
 socket.on("connect", () =>{
     console.log(socket.connected)
+})
+
+socket.on("broadcast", (arg) =>{
+    const message = document.createElement("p")
+    message.textContent = (arg)
+    messageChain.append(message)
+})
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    console.log(e.target.message.value)
+    socket.emit("message", e.target.message.value)
 })

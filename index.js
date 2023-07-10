d35ba9a94e61ae1c7b4c1e6c07602b340859ec21
Matt Eva <matteweva@gmail.com>
2023-07-09 19:18:06 -0700

    achieved multiple room setup

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-const socket = io("http://localhost:4000", {
-    query: {
-        key: ""
-    }
-})
+let socket;
 const form = document.getElementById("message-form")
 const messageChain = document.getElementById("message-chain")
+const roomB = document.getElementById("room-b")
+const roomA = document.getElementById("room-a")
+console.log(roomB)
 
-socket.on("connect", () =>{
-    console.log(socket.connected)
+const connectSocket = (room) =>{
+   socket = io("http://localhost:4000", {
+        query: {
+            room: room
+        }
+    })
+    socket.on("connect", () =>{
+        console.log(socket.connected)
+    })
+    
+    socket.on("room", (arg) =>{
+        console.log(arg)
+    })
+    
+    socket.on("broadcast", (arg) =>{
+        console.log("receiving broadcast")
+        const message = document.createElement("p")
+        message.textContent = (arg)
+        messageChain.append(message)
+    })
+}
+
+roomB.addEventListener("click", () =>{
+    console.log("b")
+    connectSocket("b")
 })
 
-socket.on("room", (arg) =>{
-    console.log(arg)
+roomA.addEventListener("click", () =>{
+    console.log("a")
+    connectSocket("a")
 })
 
-socket.on("broadcast", (arg) =>{
-    console.log("receiving broadcast")
-    const message = document.createElement("p")
-    message.textContent = (arg)
-    messageChain.append(message)
-})
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault()
     socket.emit("message", e.target.message.value)
 })
 
-fetch("http://localhost:4000")
-    .then(r => r.json())
-    .then(console.log)
+// fetch("http://localhost:4000")
+//     .then(r => r.json())
+//     .then(console.log)
